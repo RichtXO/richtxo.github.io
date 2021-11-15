79959e4f44612c09357a52f3ae7cda6562052609
Richard Tsai <richtxo@gmail.com>
2021-11-14 22:47:34 -0500

    fixed tab header

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      titleTemplate={defaultTitle}
       meta={[
         {
           name: `description`,
