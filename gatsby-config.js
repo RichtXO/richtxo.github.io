8ffbbad3153b7e59fbcfd3ac5b5ddee73afb6fed
Richard Tsai <richtxo@gmail.com>
2021-11-15 00:14:49 -0500

    chaning seo description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Richard Tsai`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `A personal website by Richard Tsai`,
     author: `@gatsbyjs`,
   },
   plugins: [
